a15e4ccdff37da0fd6941aa71662084e25425915
PARKINHYO <inhyopark122@gmail.com>
2021-10-06 14:12:04 +0900

    chore: vpc modules seperate test

--- main.tf
+++ main.tf
@@ -1,7 +1,25 @@
-provider "aws" {
-  region = local.region
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.28"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.0.1"
+    }
+  }
+  required_version = ">= 0.14"
+
+  backend "remote" {
+    organization = "ihp001"
+
+    workspaces {
+      name = "WordPress-Terraform"
+    }
+  }
 }
 
-locals {
-  region = "ap-northeast-3"
+provider "aws" {
+  region = var.region
 }
---
+++ variables.tf
@@ -0,0 +1,4 @@
+variable "region" {
+    default = "ap-northeast-3"
+  
+}
