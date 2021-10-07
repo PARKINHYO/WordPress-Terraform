1ebfef2a917544d7051b76ed5ea50c976b141eaf
PARKINHYO <inhyopark122@gmail.com>
2021-10-07 12:08:07 +0900

    feat: rds module

--- main.tf
+++ main.tf
@@ -1,11 +1,25 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.28"
+    }
+    # random = {
+    #   source  = "hashicorp/random"
+    #   version = "3.0.1"
+    # }
+  }
+  required_version = ">= 0.14"
+
+  backend "remote" {
+    organization = "ihp001"
+
+    workspaces {
+      name = "WordPress-Terraform"
+    }
+  }
+}
+
 provider "aws" {
   region = "ap-northeast-3"
-}
-
-module "vpc"{
-  source = "./modules/vpc"
-}
-
-module "rds" {
-  source = "./modules/rds"
 }
