0ec2b1c945ae7a11f2c3e342e457b38f395b03da
parkinhyo <inhyopark122@gmail.com>
2021-10-08 00:07:37 +0900

    chore: version error

--- main.tf
+++ main.tf
@@ -2,14 +2,14 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 3.26.0"
+      version = ">= 3.28"
     }
     # random = {
     #   source  = "hashicorp/random"
     #   version = "3.0.1"
     # }
   }
-  required_version = ">= 0.14"
+  required_version = ">= 0.12.31"
 
   backend "remote" {
     organization = "ihp001"
