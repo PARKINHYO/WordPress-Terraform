a898c221a8552b82057a392693ea6027909e9186
parkinhyo <inhyopark122@gmail.com>
2021-10-07 23:59:58 +0900

    chore: version error

--- main.tf
+++ main.tf
@@ -4,10 +4,10 @@ terraform {
       source  = "hashicorp/aws"
       version = ">= 3.26.0"
     }
-    random = {
-      source  = "hashicorp/random"
-      version = "3.0.1"
-    }
+    # random = {
+    #   source  = "hashicorp/random"
+    #   version = "3.0.1"
+    # }
   }
   required_version = ">= 0.14"
 
