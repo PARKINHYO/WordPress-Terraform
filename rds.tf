2aeb7f6ef9505f2eb121feaa1c2e713880fba188
PARKINHYO <inhyopark122@gmail.com>
2021-10-07 14:08:59 +0900

    feat: rds module

--- rds.tf
+++ rds.tf
@@ -28,6 +28,10 @@
 #   tags = local.tags
 # }
 
+provider "aws" {
+  region = "ap-northeast-3"
+}
+
 module "security_group" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "~> 4"
