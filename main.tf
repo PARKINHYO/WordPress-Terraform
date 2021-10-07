643e7fb3d4c3b977b4314469f08a9ae1e6bcc7b9
PARKINHYO <inhyopark122@gmail.com>
2021-10-07 13:53:21 +0900

    feat: rds module

--- main.tf
+++ main.tf
@@ -8,10 +8,10 @@ terraform {
       source  = "hashicorp/aws"
       version = ">= 3.28"
     }
-    random = {
-      source  = "hashicorp/random"
-      version = "3.0.1"
-    }
+    # random = {
+    #   source  = "hashicorp/random"
+    #   version = "3.0.1"
+    # }
   }
   required_version = ">= 0.14"
 
